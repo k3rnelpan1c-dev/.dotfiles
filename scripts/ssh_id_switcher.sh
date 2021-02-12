4cc47700d2081bc4c0de3bdbe67d9b3fcecdbc58
k3rnelpan1c-dev <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2021-02-12 21:23:18 +0100

    add ssh_id_switcher script

---
+++ scripts/ssh_id_switcher.sh
@@ -0,0 +1,44 @@
+#!/etc/bash
+
+# WARNING: 
+#  I designed this script to easily switch between my ssh IDs.
+#  It depends on a certain naming schema of the ssh ID keys and OVERWRITES id_ed25519 & id_ed25519.pub
+
+set -e
+
+function __prompt() {
+  echo "WARNING: About to overwrite id_ed25519 with ${1}_ed25519!"
+  echo -n "Proceed? [y/n]: "
+  read -n 1 ans
+  [[ ${ans} != 'y' ]] && exit 1
+}
+function __overwrite() {
+  if [[ -f ~/.ssh/${1}_ed25519 ]]; then
+    mv -f ~/.ssh/${1}_ed25519 ~/.ssh/id_ed25519
+  else
+    echo "ERROR: ~/.ssh/${1}_ed25519 not found! Have you set-up the environemt properly?"
+    exit 1
+  fi
+}
+
+function ssh_id_switcher() {
+  case "${1}" in
+    k3)
+      __prompt "k3"
+      __overwrite "k3"
+    ;;
+    h1)
+      __prompt "h1"
+      __overwrite "h1"
+    ;;
+    dh)
+      __prompt "dh"
+      __overwrite "dh"
+    ;;
+    *)
+      echo "Usage: ${0} {k3|h1|dh}"
+      exit 0
+    ;;
+  esac
+  echo "Successfully changed ssh ID to ${1}_ed25519!"
+}
