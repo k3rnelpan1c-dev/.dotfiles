9f950e32fde3c7c48f160b16f1d50ccf7a13a31a
k3rnelpan1c-dev <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2021-02-12 21:53:06 +0100

    fix ssh_id_switcher script

--- scripts/ssh_id_switcher.sh
+++ scripts/ssh_id_switcher.sh
@@ -8,15 +8,20 @@ function __prompt() {
   echo "WARNING: About to overwrite id_ed25519 with ${1}_ed25519!"
   echo -n "Proceed? [y/n]: "
   read -n 1 ans
-  [[ ${ans} != 'y' ]] && exit 1
+  echo ""
+  [[ ${ans} != 'y' ]] && return 1
+  return 0
 }
 function __overwrite() {
   if [[ -f ~/.ssh/${1}_ed25519 ]]; then
-    mv -f ~/.ssh/${1}_ed25519 ~/.ssh/id_ed25519
+    /bin/cp -f ~/.ssh/${1}_ed25519 ~/.ssh/id_ed25519
+    /bin/cp -f ~/.ssh/${1}_ed25519.pub ~/.ssh/id_ed25519.pub
   else
     echo "ERROR: ~/.ssh/${1}_ed25519 not found! Have you set-up the environemt properly?"
-    exit 1
+    return 1
   fi
+  echo "Successfully changed ssh ID to ${1}_ed25519!"
+  return 0
 }
 
 # # ssh_id_switcher - switches the active ssh ed25519 id
@@ -24,24 +29,16 @@ function __overwrite() {
 # # usage: ssh_id_switcher {k3|h1|dh}
 function ssh_id_switcher() {
   case "${1}" in
-    k3)
-      __prompt "k3"
-      __overwrite "k3"
-    ;;
-    h1)
-      __prompt "h1"
-      __overwrite "h1"
-    ;;
-    dh)
-      __prompt "dh"
-      __overwrite "dh"
+    k3|h1|dh)
+      __prompt "${1}"
+      [[ $? == 0 ]] && __overwrite "${1}" || return 1
+      return $?
     ;;
     *)
       echo "Usage: ${0} {k3|h1|dh}"
-      exit 0
+      return 0
     ;;
   esac
-  echo "Successfully changed ssh ID to ${1}_ed25519!"
 }
 
 alias ssh-id='ssh_id_switcher'
